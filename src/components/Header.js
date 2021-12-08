94b23503e7050ec8f0f1fead465cffecacfff804
eyob <eyob@Eyob1na.com>
2021-12-08 18:14:43 +0300

    ADD:Section for the TESLA

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+    return (
+        <div>
+            Header Test
+        </div>
+    )
+}
+
+export default Header
